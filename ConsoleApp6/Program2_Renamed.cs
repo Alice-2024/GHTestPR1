8423f4e21aa04cf16bed5afaa0b17b9c73c144a6
Alice Lei (Beyondsoft Corporation) <v-qilei@microsoft.com>
2024-11-12 08:10:36 +0000

    11

--- ConsoleApp6/Program2_Renamed.cs
+++ ConsoleApp6/Program2_Renamed.cs
@@ -5,7 +5,7 @@ namespace MyApp
     internal class Program2_Renamed
     {
         static void Main(string[] args)
-        {
+        {//1111
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
