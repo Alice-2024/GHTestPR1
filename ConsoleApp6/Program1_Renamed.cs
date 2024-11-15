b7b83d62c1fbaea2537fea0a0ee2d54a20d35ec4
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2024-11-15 09:08:12 +0000

    111

--- ConsoleApp6/Program1_Renamed.cs
+++ ConsoleApp6/Program1_Renamed.cs
@@ -1,2 +1,17 @@
+<<<<<<< Updated upstream
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+=======
+﻿using System;               
+
+namespace MyApp
+{       
+    internal class Program2_Renamed
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");//111
+        }
+    }
+}
+>>>>>>> Stashed changes
