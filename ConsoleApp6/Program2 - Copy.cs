119daf5fd20bce899351784c65f3e199bd63ffb1
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2024-11-21 06:33:09 +0000

    44

--- ConsoleApp6/Program2 - Copy.cs
+++ ConsoleApp6/Program2 - Copy.cs
@@ -13,7 +13,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");//33333
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");//44444
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
