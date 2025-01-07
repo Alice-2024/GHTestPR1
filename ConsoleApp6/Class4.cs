ce4a541da3ab927cc60dcd889be5b46a819c19c1
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-01-07 09:57:01 +0000

    111

--- ConsoleApp6/Class4.cs
+++ ConsoleApp6/Class4.cs
@@ -10,3 +10,15 @@
     {
     }
 }
+namespace ConsoleApp6
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;//aaaaaaaaabbbb
+
+    internal class Class5
+    {
+    }
+}
