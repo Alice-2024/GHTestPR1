4ccde9d4de9433f8c242ee2e400fcccd3ec188a2
Alice <v-qilei@microsoft.com>
2025-05-21 06:39:15 +0000

    111

--- ConsoleApp6/Class1.cs
+++ ConsoleApp6/Class1.cs
@@ -1,12 +1,7 @@
 ﻿namespace ConsoleApp6//111
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;//2222
-    using System.Text;
-    using System.Threading.Tasks;
-
     internal class Class1
     {
+        //aaaaaa
     }
 }
