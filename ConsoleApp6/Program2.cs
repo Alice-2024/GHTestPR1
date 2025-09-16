fd960a41b4af70420328a987ab58133d1c383f5c
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-09-16 02:00:24 +0000

    22

--- ConsoleApp6/Program2.cs
+++ ConsoleApp6/Program2.cs
@@ -1,11 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Group2");
+            Console.WriteLine("Group3");
+            Console.WriteLine("Group4");
+            Console.WriteLine("Group5");
+            Console.WriteLine("Group6");
+            Console.WriteLine("Group7");
+            Console.WriteLine("Group8");
+            Console.WriteLine("Group9");
+            Console.WriteLine("Group10");
+            Console.WriteLine("Group0");
+            Console.WriteLine("Group1");
+
+        }
+    }
+}
