c284a4409d39fc7b20a5ea0e5b45f510d5efaeed
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-09-23 11:07:32 +0800

    22

--- ConsoleApp6/Class1.cs
+++ ConsoleApp6/Class1.cs
@@ -1,13 +1,10 @@
-﻿
-{
-    using System;//1111222333344
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic; // Ensure this namespace is necessary for the code. Remove if unused.
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-    internal class Class1
+internal class Class1
 {
-}
 }
 //111111122223334444
