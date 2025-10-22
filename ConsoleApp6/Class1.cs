98b66b6b6c2f6d6fe64f2bcdbaec1347fee2165d
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-10-22 14:49:51 +0800

    11

--- ConsoleApp6/Class1.cs
+++ ConsoleApp6/Class1.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic; // Ensure this namespace is necessary for the code. Remove if unused.
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-internal class Class1
+﻿internal class Class1
 {
 }
-//Test PR comments
+// Test PR comments
+// Added a meaningful comment to explain the purpose of this class
+// This class currently does not contain any members or methods.
