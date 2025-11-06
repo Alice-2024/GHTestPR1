d8f20341a170fb5bd35e1d3463be944dca1b16c1
Alice Lei (BEYONDSOFT CONSULTING INC) <v-qilei@microsoft.com>
2025-11-06 13:53:06 +0800

    'Update Class1.cs'
      Removed unused `using` directives to clean up the code.
      Changed `Class1` class visibility from `internal` to `public`.
      Added `Method1` to `Class1` to print "Hello, World!".

--- ConsoleApp6/Class1.cs
+++ ConsoleApp6/Class1.cs
@@ -1,12 +1,10 @@
 ﻿namespace ConsoleApp6//111
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;//2222
-    using System.Text;
-    using System.Threading.Tasks;
-
     internal class Class1
     {
+        public void Method1()
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
 }
