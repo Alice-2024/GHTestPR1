7b52f2921f80a4520b94a5e49717983a246bcad2
v-qilei <v-qilei@CPC-vqile-99E3Y>
2025-12-01 10:28:27 +0800

    'Remove redundant logs'
      删除了`Program2.cs`文件中多余的`Console.WriteLine("Hello, World!");`语句，共删除了三行，减少了重复输出。

--- ConsoleApp6/Program2.cs
+++ ConsoleApp6/Program2.cs
@@ -5,6 +5,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
