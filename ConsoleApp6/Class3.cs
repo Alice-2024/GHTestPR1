c9ae8ffcc4a910ff67237a570784a46eb416eef4
v-qilei <v-qilei@CPC-vqile-99E3Y>
2025-12-11 09:58:10 +0800

    '移除using并添加注释'
      移除了顶部的using指令，并在Class3类中新增了//bbbb注释。

--- ConsoleApp6/Class3.cs
+++ ConsoleApp6/Class3.cs
@@ -1,13 +1,9 @@
 ﻿namespace ConsoleApp6
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;//11112222222222
     //aaaaaaaaaaaaaaa
     internal class Class3
     {
         //aaaa
+        //bbbb
     }
 }
